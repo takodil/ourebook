8f84a993fa240e230cf402350b724ea5c7d61c10
Aldila <aldila@arkavis.com>
2014-04-20 14:06:38 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ourebook::Application.config.secret_token = '9efe95bc4ba065f16406483d02734062aae73e74989077b2ff636d46c1ca1944be8e9f183d32246dd7c41ce9a337c22c8e78ff481eacfc30ca0f7ae70d1ebca1'
